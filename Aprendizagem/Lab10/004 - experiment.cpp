f1e7254c8aa244c2b7813600db191bb15683457f
Bird-of-Hermes <119917246+Bird-of-Hermes@users.noreply.github.com>
2022-12-11 17:10:44 -0300

    Update

--- Aprendizagem/Lab10/004 - experiment.cpp
+++ Aprendizagem/Lab10/004 - experiment.cpp
@@ -3,12 +3,12 @@
 int main()
 {   
     using namespace std;
-    int vector[247] = {10,20,30,40,50};
+    int vector[5] = {10,20,30,40,50};
 
     for(unsigned int i = 0;;i++)
     {
         unsigned int overflow;
-        vector[i] = 61;
+        vector[i] = 60;
         cout << vector[1] << "\n" << i << "=";
     }
 }
