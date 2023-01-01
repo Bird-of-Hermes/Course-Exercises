f960d9bf6bbb46ffc30de7bc607f2b9f134cac1b
Bird-of-Hermes <119917246+Bird-of-Hermes@users.noreply.github.com>
2023-01-01 16:32:02 -0300

    bool problem
    always returns true no matter what input

--- Labs/Lab26/learning/002.cpp
+++ Labs/Lab26/learning/002.cpp
@@ -9,40 +9,42 @@ void Displaying(string);
 int main()
 {
     string StudentsName;
-    
     Displaying(StudentsName);
-    
 }
 
 bool InfoCaptureAndFormat(string Names)
 {
     bool ToF = false; // true or false used to check Displaying() if inscruction
-
     cout << "\nName: ";
     getline(cin, Names);
-    if (Names.compare("end") == 0)
+    if (Names.compare("end") == 0) // ends if user enters "end"
         exit(0);
     for (size_t i = 0; i < Names.length(); i++)
     {
-        // MISSING if FILTER: TOLOWER DAS/DOS/DA/DO/DE 
-
+        // FILTER: DOESN'T UPPERCASE DAS/DOS/DA/DO/DE/etc
+        if((Names[i] == ' ' && isalpha(Names[i+1]) && isalpha(Names[i+2]) && Names[i+3] == ' ')
+         || (Names[i] == ' ' && isalpha(Names[i+1]) && isalpha(Names[i+2]) && isalpha(Names[i+3]) && Names[i+4] == ' '))
+            continue;
+        // always uppercases the first letter of the first name
         if(islower(Names[0]))
         {
             Names[0] = toupper(Names[0]);
             ToF = true;
         }
-        if ((Names[i] == ' ' || Names[i] == '\t') && (islower(Names[i+1])))
+        // checks if the first letter after a space is lowercase, then uppercases it  
+        else if ((Names[i] == ' ' || Names[i] == '\t') && (islower(Names[i+1])))
         {
             i++;
             Names[i] = toupper(Names[i]);
             ToF = true;
         }
+        // checks if the first letter after a space is uppercase, then skips it
         else if ((Names[i] == ' ' || Names[i] == '\t') && (isupper(Names[i+1])))
         {
-            i++;
+            i++; // skip part, otherwise the next check will always go true and will lowercase everything
             continue;
         }
-        else if(isupper(Names[i]))
+        else if(isupper(Names[i])) // turns every letter that is upper that doesn't fit above into lower
         {
             Names[i] = tolower(Names[i]);
             ToF = true;
@@ -50,7 +52,7 @@ bool InfoCaptureAndFormat(string Names)
     }
         cout << Names;
 
-    return ToF;
+    return ToF; // returns true if any letter was fixed
 }
 
 void Displaying (string Students)
